8fcfe69d9a9f2a728c2743276fb335def017b5c0
VictorVaCa <a17vicvalcam@inspedralbes.cat>
2024-10-21 12:18:23 +0200

    Practica finalitzada

--- app/src/main/java/com/example/victor_vallecillos_uf1_act6/MainActivity.kt
+++ app/src/main/java/com/example/victor_vallecillos_uf1_act6/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.victor_vallecillos_uf1_act6
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editText = findViewById<android.widget.EditText>(R.id.editText)
+        val button = findViewById<android.widget.Button>(R.id.button)
+
+        button.setOnClickListener {
+            val text = editText.text.toString()
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
         }
     }
 }
